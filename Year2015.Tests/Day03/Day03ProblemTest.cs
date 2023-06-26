5479ff3b98eae445fc05721c77fb3dfb750f5732
vlad.povarna <vlad.povarna@uipath.com>
2023-06-26 12:15:35 +0300

    AoC2015 Day03 Part 1 Solution

---
+++ Year2015.Tests/Day03/Day03ProblemTest.cs
@@ -0,0 +1,23 @@
+﻿using Xunit.Abstractions;
+using Year2015.Day03;
+using Year2015.Tests.Utils;
+
+namespace Year2015.Tests.Day03;
+
+public class Day03ProblemTest
+{
+    private readonly ITestOutputHelper _testOutputHelper;
+
+    public Day03ProblemTest(ITestOutputHelper testOutputHelper)
+    {
+        _testOutputHelper = testOutputHelper;
+    }
+    [Fact]
+    public void Part1Solution()
+    {
+        var problem = new Problem();
+        var input = FilePathUtil.ReadInputAsString(3, "input.txt");
+        var solution= problem.Part1(input);
+        _testOutputHelper.WriteLine($"{solution}");
+    }
+}
---
+++ Year2015/Day03/Problem.cs
@@ -0,0 +1,28 @@
+﻿namespace Year2015.Day03;
+
+public class Problem
+{
+    public int Part1(string input)
+    {
+        var visited = new HashSet<(int, int)> { (0, 0) };
+        (int irow, int icol) currentPosition = (0, 0);
+        foreach (var ch in input)
+        {
+            switch (ch)
+            {
+                case '^': currentPosition.irow-- ; break;
+                case '<': currentPosition.icol--; break;
+                case '>': currentPosition.icol++; break;
+                case 'v': currentPosition.irow++; break;
+            }
+            visited.Add(currentPosition);
+        }
+        return visited.Count;
+    }
+
+    public int Part2(string input)
+    {
+        return -1;
+    }
+    
+}
