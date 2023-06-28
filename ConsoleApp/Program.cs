4f53121387aad319216dab703e455df3e63b20b2
vlad.povarna <vlad.povarna@uipath.com>
2023-06-28 14:34:19 +0300

    AoC2015, Day06 Solutions

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,12 +1,2 @@
 ﻿// See https://aka.ms/new-console-template for more information
-
-using System.Data;
-
-Console.WriteLine("Hello, World!");
-
-var actors = 1;
-(int irow, int icol)[] valueTuples = new(int irow, int icol)[actors];
-Console.WriteLine(valueTuples[0].irow);
-Console.WriteLine(valueTuples[0].icol);
-
-Console.WriteLine("Test String");
+Console.WriteLine("Hello AOC2015!");
