dda07cbe96768263b923c029c971c83c4460853d
vlad.povarna <vlad.povarna@uipath.com>
2023-07-31 18:00:06 +0300

    AoC 2015, Day7 Part1 and Part2 solutions

---
+++ Year2015.Tests/Day07/Day07ProblemTest.cs
@@ -0,0 +1,31 @@
+﻿using Xunit.Abstractions;
+using Year2015.Day07;
+using Year2015.Tests.Utils;
+
+namespace Year2015.Tests.Day07;
+
+public class Day07ProblemTest
+{
+    private readonly ITestOutputHelper _testOutputHelper; 
+    private readonly Problem _problem = new();
+    private readonly List<String> _lines = FilePathUtil.ReadInputAsListOfString(7);
+
+    public Day07ProblemTest(ITestOutputHelper testOutputHelper)
+    {
+        _testOutputHelper = testOutputHelper;
+    }
+
+    [Fact]
+    public void Part1Solution()
+    {
+        var result = _problem.Part1(_lines);
+        _testOutputHelper.WriteLine($"AOC2015, Day07, Part1 solution result: {result}");
+    }
+    
+    [Fact]
+    public void Part2Solution()
+    {
+        var result = _problem.Part2(_lines);
+        _testOutputHelper.WriteLine($"AOC2015, Day07, Part2 solution result: {result}");
+    }
+}
---
+++ Year2015/Day07/Problem.cs
@@ -0,0 +1,17 @@
+﻿using Year2015.Day07;
+
+namespace Year2015.Day07;
+
+public class Problem
+{
+    public int Part1(List<string> lines)
+    {
+        var calc = InputParser.Parse(lines);
+        return calc["a"](new InputParser.State());
+    }
+
+    public int Part2(List<string> lines)
+    {
+        var calc = InputParser.Parse(lines);
+        return calc["a"](new InputParser.State { ["b"] = calc["a"](new InputParser.State()) });    }
+}
