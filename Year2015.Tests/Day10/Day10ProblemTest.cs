387ca227671c5bb2aa421df8b7ea8c7f6ce7ca5c
vlad.povarna <vlad.povarna@uipath.com>
2023-08-05 10:47:30 +0300

    AoC 2015, Day10 Part1 & Part2 solutions

---
+++ Year2015.Tests/Day10/Day10ProblemTest.cs
@@ -0,0 +1,30 @@
+﻿using Xunit.Abstractions;
+using Year2015.Day10;
+using Year2015.Tests.Utils;
+
+namespace Year2015.Tests.Day10;
+
+public class Day10ProblemTest
+{
+    private readonly ITestOutputHelper _testOutputHelper; 
+    private readonly Problem _problem = new();
+
+    public Day10ProblemTest(ITestOutputHelper testOutputHelper)
+    {
+        _testOutputHelper = testOutputHelper;
+    }
+
+    [Fact]
+    public void Part1Solution()
+    {
+        var result = _problem.Part1("3113322113");
+        _testOutputHelper.WriteLine($"AOC2015, Day10, Part1 solution result: {result}");
+    }
+    
+    [Fact]
+    public void Part2Solution()
+    {
+        var result = _problem.Part2("3113322113");
+        _testOutputHelper.WriteLine($"AOC2015, Day10, Part2 solution result: {result}");
+    }
+}
