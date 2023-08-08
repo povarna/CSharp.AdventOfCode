4f4fab401c8fedbf26bdc38042196b75fc48b6bf
vlad.povarna <vlad.povarna@uipath.com>
2023-08-08 10:08:06 +0300

    AoC 2015, day11 Part1 & Part2 solutions

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,12 +1,3 @@
 ﻿// See https://aka.ms/new-console-template for more information
-
-using Year2015.Day11;
-
 Console.WriteLine("Hello AOC2015!");
-
-var problem = new Problem();
-
-var solution = problem.Part1("hxbxwxba");
-System.Console.WriteLine(solution);
-
     
---
+++ Year2015.Tests/Day11/Day11ProblemTest.cs
@@ -0,0 +1,29 @@
+﻿using Xunit.Abstractions;
+using Year2015.Day11;
+
+namespace Year2015.Tests.Day11;
+
+public class Day11ProblemTest
+{
+    private readonly ITestOutputHelper _testOutputHelper; 
+    private readonly Problem _problem = new();
+
+    public Day11ProblemTest(ITestOutputHelper testOutputHelper)
+    {
+        _testOutputHelper = testOutputHelper;
+    }
+
+    [Fact]
+    public void Part1Solution()
+    {
+        var result = _problem.Part1("hxbxwxba");
+        _testOutputHelper.WriteLine($"AOC2015, Day11, Part1 solution result: {result}");
+    }
+    
+    [Fact]
+    public void Part2Solution()
+    {
+        var result = _problem.Part2("hxbxwxba");
+        _testOutputHelper.WriteLine($"AOC2015, Day11, Part2 solution result: {result}");
+    }
+}
