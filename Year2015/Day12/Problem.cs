5afffb93df7e84e0ae2322034b76bd43ffbd7d30
vlad.povarna <vlad.povarna@uipath.com>
2023-08-08 20:48:40 +0300

    AoC 2015, Day 12, Part1 & Part2 solutions

---
+++ Year2015/Day12/Problem.cs
@@ -0,0 +1,34 @@
+﻿using System.Text.Json;
+
+namespace Year2015.Day12;
+
+public class Problem
+{
+    public int Part1(string input)
+    {
+        return Solve(input, false);
+    }
+
+    public int Part2(string input)
+    {
+        return Solve(input, true);
+    }
+
+    private int Solve(string input, bool skipRed)
+    {
+        int Traverse(JsonElement jsonElement)
+        {
+            return jsonElement.ValueKind switch
+            {
+                JsonValueKind.Object when skipRed && jsonElement.EnumerateObject().Any(t =>
+                    t.Value.ValueKind == JsonValueKind.String && t.Value.GetString() == "red") => 0,
+                JsonValueKind.Object => jsonElement.EnumerateObject().Select(t => Traverse(t.Value)).Sum(),
+                JsonValueKind.Array => jsonElement.EnumerateArray().Select(Traverse).Sum(),
+                JsonValueKind.Number => jsonElement.GetInt32(),
+                _ => 0
+            };
+        }
+
+        return Traverse(JsonDocument.Parse(input).RootElement);
+    }
+}
