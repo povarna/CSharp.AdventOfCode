82bf3e8f0215cac21e597575e8865a169d84d8ee
povarna <134229701+povarna@users.noreply.github.com>
2023-08-26 23:47:57 +0300

    AOC 2015, day15, part1 & part2 solutions (#3)

---
+++ Year2015/Day15/Problem.cs
@@ -0,0 +1,116 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Year2015.Day15;
+
+public class Problem
+{
+    public int Part1(string input)
+    {
+        var ingredientsProperties = input.Split("\n")
+            .Select(CreateIngredientsProperties)
+            .ToList();
+
+        return MixIngredients(ingredientsProperties, null).ToArray().Max();
+    }
+
+    public int Part2(string input)
+    {
+        var ingredientsProperties = input.Split("\n")
+            .Select(CreateIngredientsProperties)
+            .ToList();
+
+        return MixIngredients(ingredientsProperties, 500).ToArray().Max();
+    }
+
+    private static int[] CreateIngredientsProperties(string line)
+    {
+        var regex =
+            Regex.Match(
+                input: line,
+                pattern: @"(.*): capacity (.*), durability (.*), flavor (.*), texture (.*), calories (.*)"
+            );
+        if (regex.Groups.Count != 7)
+        {
+            throw new ArgumentException($"Invalid input. Can't parse the input line: {line}");
+        }
+
+        return regex.Groups.Cast<Group>()
+            .Skip(2)
+            .Select(g => int.Parse(g.Value))
+            .ToArray();
+    }
+
+    private static IEnumerable<int> MixIngredients(IReadOnlyList<int[]> ingredientsPropertiesList, int? calories)
+    {
+        const int amountOfIngredients = 100;
+        List<int> ingredientsWeight = new();
+
+        var numberOfIngredients = ingredientsPropertiesList.Count;
+        var numberOfProperties = ingredientsPropertiesList[0].Length;
+
+        foreach (var partition in Partition(amountOfIngredients, numberOfIngredients))
+        {
+            var partitionLength = partition.Length;
+            var propsArray = new int[partitionLength, numberOfProperties];
+
+            for (var i = 0; i < partitionLength; i++)
+            {
+                var currentIngredient = ingredientsPropertiesList[i];
+                var count = partition[i];
+                for (var j = 0; j < currentIngredient.Length; j++)
+                {
+                    propsArray[i, j] += currentIngredient[j] * count;
+                }
+            }
+
+            var totalScores = CalculateTotalScore(numberOfProperties, partitionLength, propsArray);
+
+            if (calories.HasValue && totalScores.Last() != calories.Value) continue;
+            
+            var totalScore = CalculateTotalScore(totalScores);
+            if (totalScore > 0 ) ingredientsWeight.Add(totalScore);
+        }
+
+        return ingredientsWeight;
+    }
+
+    private static int CalculateTotalScore(IEnumerable<int> totalScores)
+    {
+        return totalScores
+            .SkipLast(1)
+            .ToArray()
+            .Aggregate(1, (current, a) => current * (a < 0 ? 0 : a));
+    }
+
+    private static int[] CalculateTotalScore(int numberOfProperties, int partitionLength, int[,] propsArray)
+    {
+        var accArray = new int[numberOfProperties];
+        for (var t = 0; t < numberOfProperties; t++)
+        {
+            for (var p = 0; p < partitionLength; p++)
+            {
+                accArray[t] += propsArray[p, t];
+            }
+        }
+
+        return accArray;
+    }
+
+    private static IEnumerable<int[]> Partition(int n, int k)
+    {
+        if (k == 1)
+        {
+            yield return new[] { n };
+        }
+        else
+        {
+            for (var i = 0; i <= n; i++)
+            {
+                foreach (var rest in Partition(n - i, k - 1))
+                {
+                    yield return rest.Append(i).ToArray();
+                }
+            }
+        }
+    }
+}
