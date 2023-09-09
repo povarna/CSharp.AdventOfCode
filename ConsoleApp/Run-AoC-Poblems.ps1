62cd62f916a451a04f7b8402db8014d111e6a967
vlad.povarna <vlad.povarna@uipath.com>
2023-09-09 14:31:39 +0300

    @trivial refactor

---
+++ ConsoleApp/Run-AoC-Poblems.ps1
@@ -0,0 +1,22 @@
+﻿#TODO: Replace this with an argument
+$year = 2015
+$maxDay = 21
+$parts = 1,2
+$separator = "="*50
+
+$dotnetVersion="net6.0"
+
+$currentLocation=Get-Location
+
+Set-Location -Path $currentLocation/bin/Debug/$dotnetVersion
+
+Write-Host($separator)
+for($i=1; $i -le $maxDay; $i++){
+  foreach ($part in $parts)
+  {
+    Invoke-Expression -Command "dotnet ConsoleApp.dll solve -y $year -d $i -p $part"
+  }
+  Write-Host($separator)
+}
+Set-Location -Path $currentLocation
+
--- README.md
+++ README.md
@@ -8,5 +8,5 @@ dotnet ConsoleApp.dll -y 2015 -d 1 -p 1 -p 2
 
 Run all the problems from a specific year:
 ```
-ConsoleApp\Run_all_year_problems.ps1   
+ConsoleApp\Run-AoC-Problems.ps1   
 ```
