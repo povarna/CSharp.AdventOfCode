546d96f5f139e9cb1c7ae878789e954c5f23093a
vlad.povarna <vlad.povarna@uipath.com>
2023-09-09 19:32:11 +0300

    AoC 2015, day25 part1 solution

---
+++ ConsoleApp/Year2015/Day25/Problem.cs
@@ -0,0 +1,29 @@
+﻿using System.Text.RegularExpressions;
+
+namespace ConsoleApp.Year2015.Day25;
+
+public class Problem
+{
+    public object Part1(string input) {
+        var m = 20151125L;
+        var (irow, icol) = (1, 1);
+        var (irowDst, icolDst) = Parse(input);
+        while (irow != irowDst || icol != icolDst) {
+            irow--;
+            icol++;
+            if (irow == 0) {
+                irow = icol;
+                icol = 1;
+            }
+            m = (m * 252533L) % 33554393L;
+        }
+        return m;
+    }
+
+    private static (int irowDst, int icolDist) Parse(string input)
+    {
+        var m = Regex.Match(input,
+            @"To continue, please consult the code grid in the manual.  Enter the code at row (\d+), column (\d+).");
+        return (int.Parse(m.Groups[1].Value), int.Parse(m.Groups[2].Value));
+    }
+}
