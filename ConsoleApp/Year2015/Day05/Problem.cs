08f8c8283dae0071eae71e8e30863f8c4ada2d33
povarna <134229701+povarna@users.noreply.github.com>
2023-09-09 14:19:27 +0300

    Refactor program to use System.CommandLine (#4)
    * Refactor program to use System.CommandLine library

---
+++ ConsoleApp/Year2015/Day05/Problem.cs
@@ -0,0 +1,19 @@
+﻿namespace ConsoleApp.Year2015.Day05;
+
+public class Problem
+{
+    public int Part1(string input) =>
+        input.Split("\n")
+            .Count(word =>
+            word.ContainsAtLeastOnceLetterTwice() &&
+            word.ContainsAtLeastThreeVowels() &&
+            word.DoesntContainsBackListLetterGroups()
+        );
+
+    public int Part2(string input) =>
+        input.Split("\n")
+            .Count(word =>
+            word.HasLetterWhichRepeatsWithExactlyOneLetterBetweenThem() &&
+            word.HasLetterPair()
+        );
+}
---
+++ ConsoleApp/Year2015/Day11/Problem.cs
@@ -0,0 +1,57 @@
+namespace ConsoleApp.Year2015.Day11;
+
+public class Problem
+{
+    public string Part1(string input)
+    {
+        foreach (var password in Words(input))
+        {
+            if (password.IsPasswordValid())
+            {
+                return password;
+            }
+        }
+
+        return string.Empty;
+    }
+
+    public string Part2(string input)
+    {
+        var firstValidPassword = Part1(input);
+        foreach (var password in Words(firstValidPassword))
+        {
+            if (password.IsPasswordValid())
+            {
+                return password;
+            }
+        }
+
+        return string.Empty;
+    }
+
+    private IEnumerable<string> Words(string word)
+    {
+        while (true)
+        {
+            var sb = new System.Text.StringBuilder();
+            for (var i = word.Length - 1; i >= 0; i--)
+            {
+                var ch = word[i] + 1;
+                if (ch > 'z')
+                {
+                    ch = 'a';
+                    sb.Insert(0, (char)ch);
+                }
+                else
+                {
+                    sb.Insert(0, (char)ch);
+                    sb.Insert(0, word.Substring(0, i));
+                    i = 0;
+                }
+            }
+
+            word = sb.ToString();
+            yield return word;
+        }
+    }
+}
