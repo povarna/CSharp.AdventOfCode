9391a823c825a615d796f2d3a73de3fd40c891d4
vlad.povarna <vlad.povarna@uipath.com>
2023-09-26 08:44:59 +0300

    AoC 2021, Day 1 problem, par1 & part2 solutions

---
+++ ConsoleApp/Year2021/Day01/Problem.cs
@@ -0,0 +1,41 @@
+﻿namespace ConsoleApp.Year2021.Day01;
+
+public class Problem
+{
+    public int Part1(string input)
+    {
+        var measurements = ParseInput(input);
+        return CalculateGtValues(measurements);
+    }
+
+    public int Part2(string input)
+    {
+        var measurements = ParseInput(input);
+        var acc = new List<int>();
+        for (var i = 0; i < measurements.Length - 2; i++)
+        {
+            var partialSum = 0;
+            for (var j = i; j <= i + 2; j++)
+            {
+                partialSum += measurements[j];
+            }
+
+            acc.Add(partialSum);
+        }
+
+        return CalculateGtValues(acc);
+    }
+
+    private static int CalculateGtValues(IReadOnlyList<int> measurements) =>
+        Enumerable
+            .Range(1, measurements.Count - 1)
+            .Aggregate(0, (increaseCount, i) =>
+                increaseCount + (measurements[i] > measurements[i - 1] ? 1 : 0)
+            );
+
+
+    private static int[] ParseInput(string input) =>
+        input.Split("\n")
+            .Select(int.Parse)
+            .ToArray();
+}
