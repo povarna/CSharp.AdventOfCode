c1662d4a018cedfebe5cf32e82fe605eb710296a
vlad.povarna <vlad.povarna@uipath.com>
2023-09-28 22:24:59 +0300

    AoC2021 Day5 Part2 Solution

--- AoC/Year2021/Day05/Problem.cs
+++ AoC/Year2021/Day05/Problem.cs
@@ -1,24 +1,22 @@
 ﻿using System.Text.RegularExpressions;
-using AoC.Utils;
 
 namespace AoC.Year2021.Day05;
 
 public class Problem
 {
-    public int Part1(string input)
+    public int Part1(string input) => GetBoard(input).CountOverlapPoints();
+
+    public int Part2(string input) => GetBoard(input, true).CountOverlapPoints();
+
+    private static Board GetBoard(string input, bool withDiagonals = false)
     {
         var instructions = ParseInput(input);
         var boardSize = instructions
             .Select(instruction => instruction.MaxCoordinate())
             .Max();
-        var bord = new Board(boardSize);
-        bord.UpdateGrid(instructions);
-        return bord.CountOverlapPoints();
-    }
-
-    public int Part2(string input)
-    {
-        return -1;
+        var bord = new Board(boardSize + 1);
+        bord.UpdateGrid(instructions, withDiagonals);
+        return bord;
     }
 
     private static List<Instruction> ParseInput(string input) =>
@@ -46,26 +44,79 @@ public class Problem
             _grid = new int[size, size];
         }
 
-        public void UpdateGrid(List<Instruction> instructions)
+        public void UpdateGrid(List<Instruction> instructions, bool withDiagonals = false)
         {
             foreach (var instruction in instructions)
             {
-                if (instruction.From.Y == instruction.To.Y)
+                var fromX = instruction.From.X;
+                var toX = instruction.To.X;
+                var fromY = instruction.From.Y;
+                var toY = instruction.To.Y;
+
+                //Case: 0,9 -> 5,9
+                if (fromY == toY)
                 {
-                    var startPoint = Math.Min(instruction.From.X, instruction.To.X);
-                    var endPoint = Math.Max(instruction.From.X, instruction.To.X);
+                    var startPoint = Math.Min(fromX, toX);
+                    var endPoint = Math.Max(fromX, toX);
                     for (var i = startPoint; i <= endPoint; i++)
                     {
-                        _grid[instruction.From.Y, i] += 1;
+                        _grid[fromY, i] += 1;
                     }
                 }
-                else if (instruction.From.X == instruction.To.X)
+                //Case: 2,2 -> 2,1
+                else if (fromX == toX)
                 {
-                    var startPoint = Math.Min(instruction.From.Y, instruction.To.Y);
-                    var endPoint = Math.Max(instruction.From.Y, instruction.To.Y);
-                    for (var j = startPoint; j <= endPoint; j++)
+                    var startPoint = Math.Min(fromY, toY);
+                    var endPoint = Math.Max(fromY, toY);
+                    for (var i = startPoint; i <= endPoint; i++)
                     {
-                        _grid[j, instruction.From.X] += 1;
+                        _grid[i, fromX] += 1;
+                    }
+                }
+
+                if (!withDiagonals) continue;
+                //Case: 8,0 -> 0,8
+                if (fromX == fromY && toX == toY)
+                {
+                    var startPoint = Math.Min(fromX, toX);
+                    var endPoint = Math.Max(fromX, toX);
+                    for (var i = startPoint; i <= endPoint; i++)
+                    {
+                        _grid[i, i] += 1;
+                    }
+                }
+                //Case: 0,0 -> 8,8
+                else if (fromX == toY && fromY == toX)
+                {
+                    var startPoint = Math.Min(fromX, fromY);
+                    var endPoint = Math.Max(toX, toY);
+                    var diff = endPoint - startPoint;
+                    for (var i = 0; i <= diff; i++)
+                    {
+                        _grid[startPoint + i, endPoint - i] += 1;
+                    }
+                }
+                //Case: 5,5 -> 8,2
+                else if (fromX > toX && fromY < toY || fromX < toX && fromY > toY)
+                {
+                    var x = Math.Max(fromX, toX);
+                    var y = Math.Min(fromY, toY);
+                    var diff = Math.Abs(fromX - toX);
+                    for (var i = 0; i <= diff; i++)
+                    {
+                        _grid[y + i, x - i] += 1;
+                    }
+                }
+                // Case: 6,4 -> 2,0
+                else if (fromX > toX && fromY > toY || fromX < toX && fromY < toY)
+                {
+                    var y = Math.Min(fromX, toX);
+                    var x = Math.Min(fromY, toY);
+
+                    var diff = Math.Abs(fromX - toX);
+                    for (var i = 0; i <= diff; i++)
+                    {
+                        _grid[x + i, y + i] += 1;
                     }
                 }
             }
@@ -79,8 +130,6 @@ public class Problem
                 total += (_grid[i, j] > 1) ? 1 : 0;
             return total;
         }
-
-        public void PrintGrid() => _grid.PrintGrid();
     }
 
     private sealed record Coordinate(int X, int Y)
