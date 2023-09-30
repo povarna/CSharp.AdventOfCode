9981d01382b59533f91220b2e1000571fa8cd7f5
vlad.povarna <vlad.povarna@uipath.com>
2023-09-30 22:56:13 +0300

    AoC 2021, Day8, Part1 solution

---
+++ AoC/Year2021/Day08/Problem.cs
@@ -0,0 +1,21 @@
+﻿namespace AoC.Year2021.Day08;
+
+public class Problem
+{
+    public int Part1(string input)
+    {
+        var validLengthPatterns = new HashSet<int> { 2, 3, 4, 7 };
+        return input.Split("\n")
+            .Select(line => line.Split("|"))
+            .Select(parts => parts[1])
+            .Select(patterns => patterns.Split(" ", StringSplitOptions.RemoveEmptyEntries))
+            .SelectMany(x => x)
+            .Select(pattern => pattern.Trim())
+            .Count(pattern => validLengthPatterns.Contains(pattern.Length));
+    }
+
+    public int Part2(string input)
+    {
+        return -1;
+    }
+}
