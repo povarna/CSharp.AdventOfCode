0413c6e8a38a6edf35078cb98c0986b5b809e228
vlad.povarna <vlad.povarna@uipath.com>
2023-09-30 13:00:26 +0300

    AoC2021 Day7 Part2 solution

--- AoC/Year2021/Day07/Problem.cs
+++ AoC/Year2021/Day07/Problem.cs
@@ -2,17 +2,14 @@
 
 public class Problem
 {
-    public int Part1(string input) => CalculateMinFuel(input);
+    public int Part1(string input) => CalculateMinFuel(input, false);
 
-    public int Part2(string input)
-    {
-        return -1;
-    }
+    public int Part2(string input) => CalculateMinFuel(input, true);
 
-    private static int CalculateMinFuel(string input)
+    private static int CalculateMinFuel(string input, bool additionalDistanceCost)
     {
         var positions = input.Split(",")
-            .Select(s =>s.Trim())
+            .Select(s => s.Trim())
             .Select(int.Parse)
             .ToArray();
 
@@ -21,7 +18,15 @@ public class Problem
 
         for (var i = 0; i < maxPosition; i++)
         {
-            var fuel = positions.Sum(position => Math.Abs(i - position));
+            /*
+             *  dist + (dist + 1) / 2 is the mathematical formula for summing all the numbers from 1 to dist
+             *    Ex: dist = 4. (1 + 2 + 3 + 4 = 10) equivalent with (4 * 5 / 2)
+             */
+            var fuel = positions
+                .Select(position => Math.Abs(i - position))
+                .Select(dist => additionalDistanceCost ? dist * (dist + 1) / 2 : dist)
+                .Sum();
+
             leastFuel = Math.Min(leastFuel, fuel);
         }
 
