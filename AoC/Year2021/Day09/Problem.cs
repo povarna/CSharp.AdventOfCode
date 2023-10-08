7c39aa7a287c2d139af6390596a166ea75ea3cf0
vlad.povarna <vlad.povarna@uipath.com>
2023-10-08 20:08:39 +0300

    Refactor + AoC2021 Day 9 part 2 solution

--- AoC/Year2021/Day09/Problem.cs
+++ AoC/Year2021/Day09/Problem.cs
@@ -1,18 +1,76 @@
-﻿namespace AoC.Year2021.Day09;
+﻿using System.Collections.Immutable;
+
+namespace AoC.Year2021.Day09;
 
 public class Problem
 {
     public int Part1(string input)
     {
-        var grid = ParseInput(input);
-        var list = GetMinPoints(grid);
-        return list.Sum(coordinate => grid[coordinate.x][coordinate.y]) + list.Count;
+        var map = GetMap(input);
+        return GetLowPoints(map).Select(point => 1 + map[point]).Sum();
     }
 
     public int Part2(string input)
     {
-        return -1;
+        var map = GetMap(input);
+        return GetLowPoints(map)
+            .Select(p => BasinSize(map, p))
+            .OrderByDescending(basinSize => basinSize)
+            .Take(3)
+            .Aggregate(1, (m, basinSize) => m * basinSize);
     }
+
+    private static ImmutableDictionary<(int x, int y), int> GetMap(string input)
+    {
+        var map = input.Split("\n").Select(line => line.Trim()).ToArray();
+        var dict = new Dictionary<(int, int), int>();
+
+        foreach (var i in Enumerable.Range(0, map.Length))
+        {
+            foreach (var j in Enumerable.Range(0, map[0].Length))
+            {
+                dict.Add((i, j), map[i][j] - '0');
+            }
+        }
+
+        return dict.ToImmutableDictionary();
+    }
+
+    private static int BasinSize(ImmutableDictionary<(int x, int y), int> map, (int x, int y) point)
+    {
+        var filled = new HashSet<(int x, int y)> { point };
+        var queue = new Queue<(int x, int y)>(filled);
+
+        while (queue.Any())
+        {
+            foreach (var neighbour in Neighbours(queue.Dequeue()).Except(filled))
+            {
+                if (map.GetValueOrDefault(neighbour, 9) != 9)
+                {
+                    queue.Enqueue(neighbour);
+                    filled.Add(neighbour);
+                }
+            }
+        }
+
+        return filled.Count;
+    }
+
+    private static IEnumerable<(int x, int y)> GetLowPoints(ImmutableDictionary<(int x, int y), int> map) =>
+        map.Keys
+            .Where(point => 
+                Neighbours(point)
+                    .All(neighbour => map[point] < map.GetValueOrDefault(neighbour, 9)))
+            .ToList();
+
+    private static IEnumerable<(int x, int y)> Neighbours((int x, int y) point) =>
+        new[]
+        {
+            (point.x - 1, point.y),
+            (point.x + 1, point.y),
+            (point.x, point.y - 1),
+            (point.x, point.y + 1)
+        };
 
     private static int[][] ParseInput(string input) =>
         input.Split("\n")
@@ -22,94 +80,4 @@ public class Problem
                     .ToArray()
             )
             .ToArray();
-
-    private static List<(int x, int y)> GetMinPoints(int[][] grid)
-    {
-        var minPointsList = new List<(int x, int y)>();
-
-        var m = grid.Length;
-        var n = grid[0].Length;
-
-        Console.WriteLine($"Grid dimensions: {m} x {n}");
-
-        // Get all the inside matrix values
-        for (var i = 1; i < m - 1; i++)
-        {
-            for (var j = 1; j < n - 1; j++)
-            {
-                {
-                    if (grid[i][j] < grid[i][j - 1] &&
-                        grid[i][j] < grid[i - 1][j] &&
-                        grid[i][j] < grid[i + 1][j] &&
-                        grid[i][j] < grid[i][j + 1])
-                    {
-                        minPointsList.Add((i, j));
-                    }
-                }
-            }
-        }
-
-        // Get the first and the last row
-        for (var j = 1; j < n - 1; j++)
-        {
-            if (grid[0][j] < grid[0][j - 1] &&
-                grid[0][j] < grid[0][j + 1] &&
-                grid[0][j] < grid[1][j])
-            {
-                minPointsList.Add((0, j));
-            }
-
-            if (grid[m - 1][j] < grid[m - 1][j - 1] &&
-                grid[m - 1][j] < grid[m - 1][j + 1] &&
-                grid[m - 1][j] < grid[m - 2][j])
-            {
-                minPointsList.Add((m - 1, j));
-            }
-        }
-
-        // Get the first and the last column
-        for (var i = 1; i < m - 1; i++)
-        {
-            if (grid[i][0] < grid[i - 1][0] &&
-                grid[i][0] < grid[i + 1][0] &&
-                grid[i][0] < grid[i][1])
-            {
-                minPointsList.Add((i, 0));
-            }
-
-            if (grid[i][n - 1] < grid[i - 1][n - 1] &&
-                grid[i][n - 1] < grid[i + 1][n - 1] &&
-                grid[i][n - 1] < grid[i][n - 2])
-            {
-                minPointsList.Add((i, n - 1));
-            }
-        }
-
-        // Add corners
-        if (grid[0][0] < grid[1][0] &&
-            grid[0][0] < grid[0][1])
-        {
-            minPointsList.Add((0, 0));
-        }
-
-        if (grid[0][n - 1] < grid[0][n - 2] &&
-            grid[0][n - 1] < grid[1][n - 1])
-        {
-            minPointsList.Add((0, n - 1));
-        }
-
-        if (grid[m - 1][0] < grid[m - 1][1] &&
-            grid[m - 1][0] < grid[m - 2][0])
-        {
-            minPointsList.Add((m - 1, 0));
-        }
-
-        if (grid[m - 1][n - 1] < grid[m - 2][n - 1] &&
-            grid[m - 1][n - 1] < grid[m - 1][n - 2])
-        {
-            minPointsList.Add((m - 1, n - 1));
-        }
-
-        return minPointsList;
-    }
 }
