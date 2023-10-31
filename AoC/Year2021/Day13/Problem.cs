93e32ebc708a09f5c26ba06df37c4846312ebbb0
vlad.povarna <vlad.povarna@uipath.com>
2023-10-31 11:37:59 +0200

    AoC 2021, Day13, Part2 solution

--- AoC/Year2021/Day13/Problem.cs
+++ AoC/Year2021/Day13/Problem.cs
@@ -17,14 +17,28 @@ public class Problem
         return newPoints.Count;
     }
 
-    public int Part2(string input) => -1;
+    public int Part2(string input)
+    {
+        var initialPoints = GetPoints(input);
+        var folds = GetFolds(input);
+
+        var tmpPoints = folds
+            .Aggregate(initialPoints, (points, fold) =>
+                fold.direction == Direction.X
+                    ? FoldX(fold.coordinate, points)
+                    : FoldY(fold.coordinate, points));
+
+        Console.WriteLine(ToString(tmpPoints));
+
+        return -1;
+    }
 
     private static HashSet<Point> FoldY(int y, HashSet<Point> points) =>
         points
             .Select(p => p.y > y ? new Point(x: p.x, y: 2 * y - p.y) : p)
             .ToHashSet();
 
-    private static HashSet<Point> FoldX(int x, HashSet<Point> points) =>        
+    private static HashSet<Point> FoldX(int x, HashSet<Point> points) =>
         points
             .Select(p => p.x > x ? new Point(x: 2 * x - p.x, y: p.y) : p)
             .ToHashSet();
@@ -51,6 +65,24 @@ public class Problem
                     : new Fold(int.Parse(arr[1]), Direction.Y)
             )
             .ToList();
+    }
+
+    private static string ToString(HashSet<Point> d)
+    {
+        var res = "";
+        var height = d.Select(p => p.y).Max();
+        var width = d.Select(p => p.x).Max();
+        for (var y = 0; y <= height; y++)
+        {
+            for (var x = 0; x <= width; x++)
+            {
+                res += d.Contains(new Point(x, y)) ? '#' : ' ';
+            }
+
+            res += "\n";
+        }
+
+        return res;
     }
 
     private record Point(int x, int y);
