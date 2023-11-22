aaf67271b73cb801617a8684b38fde4d821102e1
vlad.povarna <vlad.povarna@uipath.com>
2023-11-22 21:19:35 +0200

    AoC2020, day10, part2 solution

--- AoC/Year2020/Day10/Problem.cs
+++ AoC/Year2020/Day10/Problem.cs
@@ -19,12 +19,26 @@ public class Problem
         return a * b;
     }
 
-    public int Part2(string input)
+    public long Part2(string input)
     {
-        return -1;
+        var jolts = ParseInput(input);
+
+        // dynamic programming with rolling variables a, b, c for the function values at i + 1, i + 2 and i + 3.
+        var a = 1L;
+        var b = 0L;
+        var c = 0L;
+        for (var i = jolts.Count - 2; i >= 0; i--) {
+            var s =  
+                (i + 1 < jolts.Count && jolts[i + 1] - jolts[i] <= 3 ? a : 0) +
+                (i + 2 < jolts.Count && jolts[i + 2] - jolts[i] <= 3 ? b : 0) +
+                (i + 3 < jolts.Count && jolts[i + 3] - jolts[i] <= 3 ? c : 0);
+            c = b;
+            b = a;
+            a = s;
+        }
+        return a;
     }
-
-
+    
     private static ImmutableList<int> ParseInput(string input)
     {
         var num = input
