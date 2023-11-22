2268541ad1aa23dffb7b72dbcae038408020ca71
vlad.povarna <vlad.povarna@uipath.com>
2023-11-22 09:56:28 +0200

    2020 Day10 and 2021 Day17 problems

---
+++ AoC/Year2020/Day10/Problem.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Immutable;
+
+namespace AoC.Year2020.Day10;
+
+public class Problem
+{
+    public int Part1(string input)
+    {
+        var nums = ParseInput(input);
+
+        var window = nums
+            .Skip(1)
+            .Zip(nums)
+            .Select(p => (current: p.First, prev: p.Second))
+            .ToList();
+        
+        var a = window.Count(t => t.current - t.prev == 1);
+        var b = window.Count(t => t.current - t.prev == 3);
+        return a * b;
+    }
+
+    public int Part2(string input)
+    {
+        return -1;
+    }
+
+
+    private static ImmutableList<int> ParseInput(string input)
+    {
+        var num = input
+            .Split("\n")
+            .Select(line => line.Trim())
+            .Select(int.Parse)
+            .OrderBy(x => x)
+            .ToList();
+
+        return ImmutableList.Create(0)
+            .AddRange(num)
+            .Add(num.Last() + 3);
+    }
+
+}
