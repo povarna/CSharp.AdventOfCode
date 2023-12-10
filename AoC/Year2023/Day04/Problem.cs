a8cd1ec02c69af303b9016fc98983fb8e4a36474
vlad.povarna <vlad.povarna@uipath.com>
2023-12-10 22:01:12 +0200

    AoC 2023 Day4

---
+++ AoC/Year2023/Day04/Problem.cs
@@ -0,0 +1,44 @@
+﻿using System.Text.RegularExpressions;
+
+namespace AoC.Year2023.Day04;
+
+public class Problem
+{
+    public int Part1(string input) => (int)(
+        from line in input.Split("\n")
+        let card = ParseCard(line)
+        where card.matches > 0
+        select Math.Pow(2, card.matches - 1)
+    ).Sum();
+
+    // Quite imperatively, just walk over the cards keeping track of the counts.
+    public int Part2(string input) {
+        var cards = input.Split("\n").Select(ParseCard).ToArray();
+        var counts = cards.Select(_ => 1).ToArray();
+        
+        foreach (var card in counts)
+        {
+            Console.WriteLine(card);
+        }
+
+        Console.WriteLine(counts.Length);
+
+        for (var i = 0; i < cards.Length; i++) {
+            var (card, count) = (cards[i], counts[i]);
+            for (var j = 0; j < card.matches; j++) {
+                counts[i + j + 1] += count;
+            }
+        }
+        return counts.Sum();
+    }
+
+    // Only the match count is relevant for a card
+    Card ParseCard(string line) {
+        var parts = line.Split(':', '|');
+        var l = from m in Regex.Matches(parts[1], @"\d+") select m.Value;
+        var r = from m in Regex.Matches(parts[2], @"\d+") select m.Value;
+        return new Card(l.Intersect(r).Count());
+    }
+}
+
+record Card(int matches);
